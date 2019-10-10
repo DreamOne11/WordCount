c490b4ee4108991c1d22003b548dde7d62c1dcc7
charmingdid <54797041+charmingdid@users.noreply.github.com>
2019-10-10 14:03:46 +0800

    First Commit

---
+++ 201731062207/homework4/homework4/BasisInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework4
+{
+    interface BasisInterface
+    {
+        void CountChar();
+        void CountWord();
+        void CountLine();
+        void CountFrequency();
+        void Output();
+    }
+}
---
+++ 201731062207/homework4/homework4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BasisFunction basisFunction = new BasisFunction();
+            basisFunction.ReadFile();
+        }
+    }
+}
